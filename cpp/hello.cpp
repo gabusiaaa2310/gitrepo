85993499bf5b175b7baf9b3b64db33daf18a00bf
gabusiaaa2310 <gabszab@vp.pl>
2017-09-25 12:35:59 +0200

    kody C++

--- cpp/hello.cpp
+++ cpp/hello.cpp
@@ -9,7 +9,21 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-	cout <<"Witaj w C++!!!";
+	// char imie; // deklaracja zmiennej znakowej
+	char imie[10]; // deklaracja zmiennej tablicowej
+    int wiek;
+    wiek= 0; //inicjacja zmiennej
+    
+    cout <<"Witaj w C++!!!" << endl;
+	cout <<"Podaj imię: ";
+    // cin >> imie;
+    cin.getline(imie, 10);
+    cout << "Cześć, " << imie << "!" << endl;
+    cin.ignore (100, '\n');
+    cout << "Ile masz lat? ";
+    cin >> wiek; 
+    cout << "Urodziłeś się w roku " << 2017 - wiek << endl;
+    
 	return 0;
 }
 
