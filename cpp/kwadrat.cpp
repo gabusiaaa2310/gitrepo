c7c700e974f8b95457e324ad18eda69e9ebc0056
gabusiaaa2310 <gabszab@vp.pl>
2017-09-26 10:38:14 +0200

    Pola figu

---
+++ cpp/kwadrat.cpp
@@ -0,0 +1,21 @@
+/*
+ * hello.cpp
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+
+    int bok= 0;
+    cout << "Podaj bok kwadratu";
+    cin >> bok;
+    cout << "Obwód wynosi" << 4 * bok << endl
+    cout << "Pole wynosi" << bok * bok << endl;
+    
+    
+	return 0;
+}
+
