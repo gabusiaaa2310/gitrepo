89393bf41da0f8c6125e83400332a9f544916f59
gabusiaaa2310 <gabszab@vp.pl>
2017-09-29 08:46:05 +0200

    C++

--- cpp/trojkat.cpp
+++ cpp/trojkat.cpp
@@ -4,17 +4,27 @@
 
 
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main(int argc, char **argv)
 {
 
     int a, b, c;
+    int obwod = 0;
+    float p = 0;
     a = b = c = 0;
-    cout << "Podaj boki: " << endl;
+    cout << "Podaj boki: " <<endl;
     cin >> a >> b >> c;
-    cout << "Obwód: " << a + b + c << endl;
-  
+    if (a+b >c && a + c > b && b + c > a ) { 
+        obwod= a + b + c;
+        cout << "Obwód wynosi : " << obwod << endl;
+        p= 0.5 * obwod;
+        cout << "Współczynnik: " << p << endl;
+        cout << "Pole: " << sqrt (p*(p-a)*(p-b)*(p-c)) << endl;
+    } else {
+        cout << "Z tych boków nie powstanie trójkąt :( ";
+        }
     
 	return 0;
 }
