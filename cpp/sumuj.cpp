57fec03f7beaeb1d42aadca1bcd0e70fcf6aed4c
gabusiaaa2310 <gabszab@vp.pl>
2017-10-02 12:35:30 +0200

    Pętla

---
+++ cpp/sumuj.cpp
@@ -0,0 +1,22 @@
+/*
+ * sumuj.cpp
+ * sumuje 10 liczb, wynik drukuje na ekranie.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int i;
+
+    for (i = 1; i < 101; i++)
+    {
+        
+    }
+
+    return 0;
+}
+
