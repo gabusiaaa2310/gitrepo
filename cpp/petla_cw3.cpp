bfd825b88ca5972df8f898469a6b65c8e19e673a
gabusiaaa2310 <gabszab@vp.pl>
2017-10-09 18:44:01 +0200

    cw4

--- cpp/petla_cw3.cpp
+++ cpp/petla_cw3.cpp
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
 	int n;
     cin>>n;
     for(int i=0; i=n; i++)
-    cout<<i*i<<endl
+    cout<<i*i<<endl;
 	return 0;
 }
 
--- cpp/petla_cw4.cxx
+++ cpp/petla_cw4.cxx
@@ -7,6 +7,12 @@ using namespace std;
 int main(int argc, char **argv)
 
 {
-	
+	for (int i=10; i<100; i++)
+	{
+		if (i%3==0&&i%2==0)
+		cout<<i<<" ";
+	}
+	system("pause");
+	return 0;
 }
 
