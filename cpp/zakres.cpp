109483c22ff04350f438765a7935c6b7aeb499d6
gabusiaaa2310 <gabszab@vp.pl>
2017-10-17 09:54:44 +0200

    zakres

--- cpp/zakres.cpp
+++ cpp/zakres.cpp
@@ -5,22 +5,25 @@
 #include <iostream>
 using namespace std;
 //int liczba, krok;//zimenne globalne
-int zwieksz() {
-    int liczba, krok;//zmienna lokalne
-    liczba=liczba+krok; 
-    return liczba;
+int zwieksz(int liczba,int krok) {
+    liczba=liczba+krok;
+    krok=3*krok; 
+    return krok;
 }
 
+void zwieksz2(int &a, int &b) {
+    a=a+b;
+    b=3*b;
+}
 int main(int argc, char **argv)
 {
     int liczba, krok;//zmienne lokalne
-    
     cout <<"Podaj liczbę i krok: ";
     cin>>liczba>>krok;
     
-    cout<<"Liczba i krok: "<<liczba<<" "<<krok<<endl;
-    //zwieksz();
-    cout <<"Liczba i krok: "<<zwieksz()<<" "<<krok<<endl;
-    return 0;
+    cout<<"Liczba i krok: "<<liczba<<" "<<&liczba<<endl;
+/*    zwieksz2(liczba, krok);
+    cout <<"Liczba i krok: "<<liczba<<" "<<krok<<endl;
+*/    return 0;
 }
 
