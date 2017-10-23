b7282ac6be21fbba8a952e713caa5d6b209fa764
gabusiaaa2310 <gabszab@vp.pl>
2017-10-23 12:28:52 +0200

    Tabele cd

--- cpp/tabele.cpp
+++ cpp/tabele.cpp
@@ -5,26 +5,34 @@
 
 #include <iostream>
 using namespace std;
-int main(int argc, char **argv)
-{
-	int liczby[10];
+
+void pobierzLiczby(int tab[], int ile) {
+    int i=0;
+    for(i=0; i<ile; i++) {
+        cout<<"Podaj liczbę: ";
+        cin>>tab[i];
+    }
+}
+void sumuj (int tab[], int ile) {
     int i=0;
     int suma=0;
-    int licznik=0;
-    
-    for(i=0; i<10; i++) {
-        cout<<"Podaj liczbę: ";
-        cin>>liczby[i];
-    }
-    
-    for(i=0; i<10; i++) {
-        //cout<<liczby[i]<<" ";
-        suma+=liczby[i];
-        if (liczby[i]%2==0)
-        licznik++;
+    for (i=0; i<ile; i++) {
+        suma+=tab[i];
     }
     cout<<"Suma liczb wynosi: "<<suma<<endl;
-    cout<<"Ilość liczb parzystych: "<<licznik;
+}
+
+int main(int argc, char **argv)
+{
+    int rozmiar=0;
+    cout<<"Ile liczb podasz?: ";
+    cin>>rozmiar;
+    
+	int liczby[rozmiar];
+        
+    pobierzLiczby(liczby,rozmiar);
+    sumuj(liczby, rozmiar);
+        
     return 0;
 }
 
