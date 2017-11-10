75bac93b804fe73b114d6f4b4b1993798829be69
gabusiaaa2310 <gabszab@vp.pl>
2017-11-10 08:46:08 +0100

    python

--- python/dlotek.py
+++ python/dlotek.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import random
 
 
@@ -9,33 +8,44 @@ def losuj(ileliczb, maksliczb):
 
     ile = 0  # ilość unikalnych liczb
     # for i in range(ileliczb):
+
     while ile < ileliczb:
         liczba = random.randint(0, maksliczb)
         if liczby.count(liczba) == 0:
             liczby.append(liczba)
             ile += 1
 
-    print(liczby)
     return liczby
 
 
-def main(args):
-    ileliczb = int(input('Ile liczb chcesz zgadywać?'))
-    maksliczb = int(input('Maksymalna losowana liczba: '))
-
-    liczby = losuj(ileliczb, maksliczb)
-
+def pobierz_typy(ileliczb):
     # pobieranie typów użytkownika
     typy = set()  # pusty zbiór
     # for i in range(ileliczb):
     ile = 0
     while ile < ileliczb:
-        typ = input("Podaj typ: ")
+        typ = int(input('Podaj typ: '))
         if typ not in typy:
             typy.add(typ)
             ile += 1
 
-        print(typy)
+    return typy
+
+
+def main(args):
+    try:
+        ileliczb = int(input('Ile liczb chcesz zgadywać?'))
+        maksliczb = int(input('Maksymalna losowana liczba: '))
+
+    while ileliczb > maksliczb or ileliczb < 1:
+        ileliczb = int(input('Ile liczb chcesz zgadnąć z %s liczb?' % maksliczb))
+
+    liczby = losuj(ileliczb, maksliczb)
+    typy = pobierz_typy(ileliczb)
+
+    trafione = set(liczby) & typy
+
+    print('Trafiłeś ', len(trafione), 'liczb')
 
     return 0
 
