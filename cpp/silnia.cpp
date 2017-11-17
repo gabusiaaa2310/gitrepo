d4b2db0ea592a9d06c8503fdd2d4afc149da773b
gabusiaaa2310 <gabszab@vp.pl>
2017-11-17 08:45:53 +0100

    nwd

--- cpp/silnia.cpp
+++ cpp/silnia.cpp
@@ -9,6 +9,14 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
+    int a;
+    int wynik=1;
+    
+    cout<<"podaj liczbę naturalną: ";
+    cin>>a;
+    
+    for(int i=1, i<=a, i++)
+     
 	
 	return 0;
 }
