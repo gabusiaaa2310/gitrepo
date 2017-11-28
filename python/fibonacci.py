9e3a7737adfd4c3c53b70c4a84a585f02c7dc82f
kl2ag1 <gabszab@vp.pl>
2017-11-28 10:40:36 +0100

    fibonaccicd

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -8,23 +8,40 @@ def fib_iter(n):
     F(1) = 1
     F(n) = F(n-2) + F(n-1) dla n > 1
     """
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
     a, b = (0, 1)
+    print(a)
     for i in range(1, n - 1):
-        a = b
-        b = a + b
-    if n > 1:
+        a, b = b, a + b
+        print (a, " ", b, " ", b / a)
+    return b
+
+
+def fib_iter2(n):
+    """Wylicza n-ty wyraz ciągu Fionacciego
+    F(1) = 1
+    F(2) = 1
+    F(n) = F(n-2) + F(n-1) dla n > 2
+    """
+    a, b = (1, 1)
+    print(a)
+    print(b)
+    for i in range(2, n):
+        a, b = a, b + b
+        print("F({:4d} / F({:4d}) = {:.5f}".format(b, a, b / a))
         return b
-    else:
-        return a
 
 
 def main(args):
-    n = int(input('Podaj wyraz ciągu: '))
-    assert fib_iter(0) == 1
-    assert fib_iter(1) == 1
-    assert fib_iter(2) == 1
-    assert fib_iter(5) == 5
-    print("Wyraz {:d} = {:d}".format(n, fib_iter(n)))
+    #  n = int(input('Podaj wyraz ciągu: '))
+    #  assert fib_iter(0) == 1
+    #  assert fib_iter(1) == 1
+    #  assert fib_iter(2) == 1
+    #  assert fib_iter(5) == 5
+    print("Wyraz {:d} = {:d}".format(10, fib_iter(10)))
     return 0
 
 
