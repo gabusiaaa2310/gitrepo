1b121697dd47e5478b32779a5ce550e67981df12
kl2ag1 <gabszab@vp.pl>
2018-01-16 10:35:52 +0100

    zlozonosc

---
+++ cpp/zlozonosc_alg1.cpp
@@ -0,0 +1,15 @@
+/*
+ * złożoność_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
---
+++ cpp/zlozonosc_alg2.cpp
@@ -0,0 +1,19 @@
+/*
+ * zlozonosc_alg2.cxx
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int a;
+    cout<<"Podaj a: "<<endl;
+    cin>>a;
+    for (int 0 < a < 100: i < i; i++)
+	
+	return 0;
+}
+
