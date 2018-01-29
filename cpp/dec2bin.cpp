73c8e86e4920503d4cba18e86eb8f94e168b0d05
kl2ag1 <gabszab@vp.pl>
2018-01-29 11:55:39 +0100

    dec2bin

---
+++ cpp/dec2bin.cpp
@@ -0,0 +1,20 @@
+/*
+ * dec2bin.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    int liczba = 120;
+    do
+    { 
+        ; 
+        
+    }while(); // napisac zeby drukowalo
+	
+	return 0;
+}
