c818f5812d157ae721aee5f772ca0e712ece0221
kl2ag1 <gabszab@vp.pl>
2018-02-01 08:46:05 +0100

    kompresja 2

---
+++ cpp/Szabat_alg1.cpp
@@ -0,0 +1,30 @@
+/*
+ * Szabat_alg1.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv
+{   
+    int a;
+    cout << "Podaj a:";
+    cin >> a;
+    do{
+        cin>>a;
+    }
+    while(0 < a && a < 100);
+    int i = 2;
+    if a == i;
+    cout<< "A jest parzyste" << endl;
+    else i++
+    
+            
+    }
+    
+        
+	return 0;
+}
+
